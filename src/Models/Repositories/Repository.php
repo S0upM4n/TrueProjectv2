6d4e1792a419b10a3619f1eaf7ff6ac7a3311666
S0upM4n <Fernandosiecm@gmail.com>
2026-03-27 20:06:41 -0300

    Update Repository.php

--- src/Models/Repositories/Repository.php
+++ src/Models/Repositories/Repository.php
@@ -5,9 +5,5 @@ namespace Morainstein\Mvc\Models\Repositories;
 
 interface Repository
 {
-  // public function add(Entity $entity) : bool;
-  // public function remove(int $id) : bool;
-  // public function update(Entity $entity) : bool;
-  // public function findById(int $entityId) : ?array;
-  // public function all() : ?array;
+
 }
